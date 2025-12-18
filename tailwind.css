ee64bdebb8637a47964a969c9be5388477c83449
mafamitha <97963990+mafamitha@users.noreply.github.com>
2025-12-18 16:31:53 +0530

    Add tailwind.css file

---
+++ tailwind.css
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "semantic-colors-primary-primary500":
+          "var(--semantic-colors-primary-primary500)",
+        "semantic-colors-typography-typography0":
+          "var(--semantic-colors-typography-typography0)",
+        "semantic-colors-typography-typography600":
+          "var(--semantic-colors-typography-typography600)",
+      },
+      fontFamily: {
+        "text-medium-lg-18px": "var(--text-medium-lg-18px-font-family)",
+        "text-medium-md-16px": "var(--text-medium-md-16px-font-family)",
+        "text-normal-lg-18px": "var(--text-normal-lg-18px-font-family)",
+        "text-normal-sm-14px": "var(--text-normal-sm-14px-font-family)",
+      },
+    },
+  },
+  plugins: [],
+};
